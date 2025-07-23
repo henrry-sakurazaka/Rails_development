17416b4328ac7315bb1e1e3275e5eda7caba1100
henrry-sakurazaka <bright.lights.21@me.com>
2025-07-23 20:02:25 +0900

    Updated ActiveSupport in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,8 @@ Rails.application.configure do
 
   # Log to STDOUT with the current request id as a default log tag.
   config.log_tags = [ :request_id ]
-  config.logger   = ActiveSupport::TaggedLogging.logger(STDOUT)
+  # config.logger   = ActiveSupport::TaggedLogging.logger(STDOUT)
+  config.logger = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
 
   # Change to "debug" to log everything (including potentially personally-identifiable information!)
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
@@ -87,4 +88,5 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.assets.initialize_on_precompile = false
 end
